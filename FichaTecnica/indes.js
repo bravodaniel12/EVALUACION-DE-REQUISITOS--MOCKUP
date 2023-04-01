4b2dfc514832925fe908a34a68e97e66bf4d99dc
bravodaniel12 <danigobra2020@gmail.com>
2023-04-01 17:48:28 -0500

    add fuentes

---
+++ FichaTecnica/indes.js
@@ -0,0 +1,13 @@
+alert(
+    "texto cailquierraa"
+    )
+
+/* duntion siempre va a ir y sirve para indicar una tarea*/
+function getficha(){
+    let producto = document.getElementById("nom_producto").value
+    document.getElementById("res-producto").innerText = producto
+}
+function getlinea(){
+    let producto = document.getElementById("li-producto").value
+    document.getElementById("lo-producto").innerText = producto
+}
