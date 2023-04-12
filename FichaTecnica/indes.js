67a70998e3d3413846f3c4d6fa9f04f23eda733f
bravodaniel12 <danigobra2020@gmail.com>
2023-04-11 20:08:47 -0500

    add fuentes

--- FichaTecnica/indes.js
+++ FichaTecnica/indes.js
@@ -1,13 +1,26 @@
 alert(
-    "texto cailquierraa"
+    "canacue"
     )
 
-/* duntion siempre va a ir y sirve para indicar una tarea*/
+/* function siempre va a ir y sirve para indicar una tarea*/
 function getficha(){
     let producto = document.getElementById("nom_producto").value
     document.getElementById("res-producto").innerText = producto
+
 }
 function getlinea(){
-    let producto = document.getElementById("li-producto").value
+    let producto = document.getElementById("li_producto").value
     document.getElementById("lo-producto").innerText = producto
+}
+function getterior(){
+    let producto = document.getElementById("te_producto").value
+    document.getElementById("to-producto").innerText = producto
+}
+function getctual(){
+    let producto = document.getElementById("pi_producto").value
+    document.getElementById("tu-producto").innerText = producto
+}
+function getmodulo(){
+    let producto = document.getElementById("cu_producto").value
+    document.getElementById("ca-producto").innerText = producto
 }
