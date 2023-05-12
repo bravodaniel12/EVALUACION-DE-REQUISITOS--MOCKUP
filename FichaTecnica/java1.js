dc1a6f04d3c7b0a009b0d07c81b8f08dde02d426
bravodaniel12 <danigobra2020@gmail.com>
2023-05-12 16:47:04 -0500

    add fuentes

--- FichaTecnica/java1.js
+++ FichaTecnica/java1.js
@@ -2,6 +2,7 @@ const a = document.querySelector("#dato1")
 const b = document.querySelector("#dato2")
 const c = document.querySelector("#dato3")
 const d = document.querySelector("#dato4")
+const dm = document.querySelector("#dato45")
 const e = document.querySelector("#dato5")
 const f = document.querySelector("#dato6")
 const g = document.querySelector("#dato7")
@@ -10,15 +11,21 @@ const i = document.querySelector("#dato9")
 const j = document.querySelector("#dato10")
 const k = document.querySelector("#dato11")
 const l = document.querySelector("#dato12")
+const m = document.querySelector("#dato13")
+const n = document.querySelector("#dato14")
 
- const link = document.querySelector('a[href="fichaT1.html"]'); 
+
+/* const p = document.querySelector("#dato16") */
+
+
 
 function getFicha(){
-    let uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez, once, doce 
+let uno, dos, tres, cuatro, cuatrom, cinco, seis, siete, ocho, nueve, diez, once, doce, trece, catorce, quince, di
 uno = document.getElementById("nom_producto").value
 dos = document.getElementById("linea").value
 tres = document.getElementById("versiones").value
 cuatro = document.getElementById("version").value
+cuatrom = document.getElementById("modulo").value
 cinco = document.getElementById("descripcion").value
 seis = document.getElementById("descripciones").value
 siete = document.getElementById("productor").value
@@ -27,12 +34,16 @@ nueve = document.getElementById("otro").value
 diez = document.getElementById("requerimiento").value
 once = document.getElementById("requerimientos").value
 doce = document.getElementById("cliente").value
+trece = document.getElementById("recomendaciones").value
+catorce = document.getElementById("funcion").value
 
-//almacenamiento local con localstorege
+
+// localstorege
 localStorage.setItem("Nombre del Producto",uno);
 localStorage.setItem("Nombre del cliente",dos);
 localStorage.setItem("Numero de Producto",tres);
 localStorage.setItem("Version de Producto",cuatro);
+localStorage.setItem("modulo",cuatrom);
 localStorage.setItem("Descripcion General",cinco);
 localStorage.setItem("Tipo de Arquitectura",seis);
 localStorage.setItem("Requerimiento físico",siete);
@@ -41,12 +52,16 @@ localStorage.setItem("Recomendaciones sistema",nueve);
 localStorage.setItem("Requerimiento sistema",diez);
 localStorage.setItem("Funciones adicionales",once);
 localStorage.setItem("Caracteristicas cliente",doce);
+localStorage.setItem("recomendaciones",trece);
+localStorage.setItem("funciones del sistema",catorce);
+
 
 //limpiando los campos o inputs
 document.getElementById("nom_producto").value = "";
 document.getElementById("linea").value = "";
 document.getElementById("versiones").value = "";
 document.getElementById("version").value = "";
+document.getElementById("modulo").value = "";
 document.getElementById("descripcion").value = "";
 document.getElementById("descripciones").value = "";
 document.getElementById("productor").value = "";
@@ -55,14 +70,19 @@ document.getElementById("otro").value = "";
 document.getElementById("requerimiento").value = "";
 document.getElementById("requerimientos").value = "";
 document.getElementById("cliente").value = "";
+document.getElementById("recomendaciones").value = "";
+document.getElementById("funcion").value = "";
+
+
 }
 
 function getcargar(){
-let uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez, once, doce 
+let uno, dos, tres, cuatro, cuatrom, cinco, seis, siete, ocho, nueve, diez, once, doce, trece, catorce, quince, di 
 uno = localStorage.getItem("Nombre del Producto");
 dos = localStorage.getItem("Nombre del cliente");
 tres = localStorage.getItem("Numero de Producto");
 cuatro = localStorage.getItem("Version de Producto");
+cuatrom = localStorage.getItem("modulo");
 cinco = localStorage.getItem("Descripcion General");
 seis = localStorage.getItem("Tipo de Arquitectura");
 siete = localStorage.getItem("Requerimiento físico");
@@ -71,12 +91,16 @@ nueve = localStorage.getItem("Recomendaciones sistema");
 diez = localStorage.getItem("Requerimiento sistema");
 once = localStorage.getItem("Funciones adicionales");
 doce = localStorage.getItem("Caracteristicas cliente");
+trece = localStorage.getItem("recomendaciones");
+catorce = localStorage.getItem("funciones del sistema");
 
-//mostrar datos almacenados
+
+//mostrar datos
 a.innerHTML = uno;
 b.innerHTML = dos;
 c.innerHTML = tres;
 d.innerHTML = cuatro;
+dm.innerHTML = cuatrom;
 e.innerHTML = cinco;
 f.innerHTML = seis;
 g.innerHTML = siete;
@@ -85,4 +109,10 @@ i.innerHTML = nueve;
 j.innerHTML = diez;
 k.innerHTML = once;
 l.innerHTML = doce;
+m.innerHTML = trece;
+n.innerHTML = catorce;
+}
+
+function clearlocalStorage(){
+    localStorage.clear();
 }
