87ff2c02023bd6d7c5eb660a0381550a30479d0d
bravodaniel12 <danigobra2020@gmail.com>
2023-06-15 20:15:15 -0500

    add fuentes

---
+++ lpp- daniel/lpp-php-java-pyton/6a.js
@@ -0,0 +1,8 @@
+function Mostrar(){
+    let euro, pesos
+
+    pesos = document.getElementById("pesos").value 
+    euro = pesos*4444
+
+    document.getElementById("euro").innerHTML = "la cantidad de euros son: "+ euro
+}
