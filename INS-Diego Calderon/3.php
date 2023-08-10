d0611063772c92a9cd48f4f782b7582d99e3eba8
bravodaniel12 <danigobra2020@gmail.com>
2023-08-10 14:46:59 -0500

    add fuentes

--- INS-Diego Calderon/3.php
+++ INS-Diego Calderon/3.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metodo Burbuja</title>
+</head>
+<body>
+   
 <?php
 function ordenarArray($arr) {
     $n = count($arr);
@@ -5,6 +14,7 @@ function ordenarArray($arr) {
     for ($i = 0; $i < $n - 1; $i++) {
         for ($j = 0; $j < $n - $i - 1; $j++) {
             if ($arr[$j] > $arr[$j + 1]) {
+                
                 // Intercambiar los elementos si están en el orden incorrecto
                 $temp = $arr[$j];
                 $arr[$j] = $arr[$j + 1];
@@ -16,10 +26,13 @@ function ordenarArray($arr) {
     return $arr;
 }
 
-// uso
-$miArray = array(42, 2, 7, 10, 9, 3);
+//  uso
+$miArray = array(34, 2, 7, 11, 9, 17);
 $miArrayOrdenado = ordenarArray($miArray);
 
-echo "Array original: " . implode(", ", $miArray) . "<br>";
+echo "Array original: " . implode(", ", $miArray) . "<br>". "<br>";
+
 echo "Array ordenado: " . implode(", ", $miArrayOrdenado);
 ?>
+</body>
+</html>
