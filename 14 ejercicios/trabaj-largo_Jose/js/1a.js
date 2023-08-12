ed62392c9de355938a730efb389e62a44b98277d
bravodaniel12 <danigobra2020@gmail.com>
2023-08-11 23:17:08 -0500

    add fuentes

--- 14 ejercicios/trabaj-largo_Jose/js/1a.js
+++
@@ -1,9 +0,0 @@
-
-function Mostrar() {
-
-    let numero
-
-    numero = document.getElementById("numero").value
-
-    document.getElementById("Numero").innerHTML = " El numero introducido es: "+ numero +" gracias"
-}
