9375c3b255c4aa67e63dd0b9163d951956ae2940
bravodaniel12 <danigobra2020@gmail.com>
2023-08-16 01:13:06 -0500

    add fuentes

--- 14 ejercicios/java y html/9a.js
+++ 14 ejercicios/java y html/9a.js
@@ -1,18 +1,21 @@
+
 function Mostrar() {
-    let segundos, segundosday, segundoshour, segundosminutes
-
-    segundos = parseInt(document.getElementById("segundos").value)
+    let segundos = parseInt(document.getElementById("segundos").value);
+    let Dms = document.getElementById("dms");
+    let errorMessage = document.getElementById("error-message");
     
+    if (segundos < 0) {
+        errorMessage.innerHTML = "Error: Ingrese un número positivo";
+        Dms.innerHTML = ""; // Clear any previous results
+    } else {
+        let segundosday = segundos / 86400;
+        let segundoshour = segundos / 3600;
+        let segundosminutes = segundos / 60;
 
-
-segundosday = (segundos/86400)
-segundoshour = (segundos/3600)
-segundosminutes = (segundos/60)
-
-Dms = document.getElementById("dms");
-Dms.innerHTML =  "La cantidad de dias que hay en los segundos ingresados son:  "+ segundosday+
-"<br>La cantidad de horas que hay en los segundos ingresados son:  "+ segundoshour+
-"<br>La cantidad de minutos que hay en los segundos ingresados son:  "+ segundosminutes+
-"<br>La cantidad de segundos son:  "+ segundos
-
+        errorMessage.innerHTML = ""; // Clear error message
+        Dms.innerHTML =  "La cantidad de días que hay en los segundos ingresados son:  " + segundosday +
+            "<br>La cantidad de horas que hay en los segundos ingresados son:  " + segundoshour +
+            "<br>La cantidad de minutos que hay en los segundos ingresados son:  " + segundosminutes +
+            "<br>La cantidad de segundos son:  " + segundos;
+    }
 }
